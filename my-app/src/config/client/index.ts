4433285b93ced538178775a91a4c7864896dae9f
POKENA7 <pokena191@gmail.com>
2024-09-10 00:40:28 +0900

    chore: Update API key configuration

---
+++ my-app/src/config/client/index.ts
@@ -0,0 +1,3 @@
+import { env } from "@/env/client.mjs";
+
+export const API_KEY = env.NEXT_PUBLIC_API_KEY;
---
+++ my-app/src/config/server/index.ts
@@ -0,0 +1,3 @@
+import { env } from "@/env/server.mjs";
+
+export const API_KEY = env.API_KEY;
